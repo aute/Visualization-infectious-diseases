2ff315922f3356ddabd7e53c44934f64d67a5592
aute <aute7527@gmail.com>
2020-03-15 18:56:12 +0800

    test2

---
+++ docs/precache-manifest.67f9e11b41607a3f06eb1edb297eff78.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce7de8918d7ecac4cc9bc68b9d925228",
+    "url": "/Visualization-infectious-diseases/index.html"
+  },
+  {
+    "revision": "cd8fe07d17f36eab8bb7",
+    "url": "/Visualization-infectious-diseases/static/css/2.970744fb.chunk.css"
+  },
+  {
+    "revision": "8f21c5901bc56c586099",
+    "url": "/Visualization-infectious-diseases/static/css/main.25d280ca.chunk.css"
+  },
+  {
+    "revision": "cd8fe07d17f36eab8bb7",
+    "url": "/Visualization-infectious-diseases/static/js/2.a9937efd.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/Visualization-infectious-diseases/static/js/2.a9937efd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f21c5901bc56c586099",
+    "url": "/Visualization-infectious-diseases/static/js/main.3e4aa7f1.chunk.js"
+  },
+  {
+    "revision": "8d585ce5032ec86a47d0",
+    "url": "/Visualization-infectious-diseases/static/js/runtime-main.b766ace7.js"
+  },
+  {
+    "revision": "e6a67d76ce9c973061ee02a65d303a19",
+    "url": "/Visualization-infectious-diseases/static/media/R0.e6a67d76.svg"
+  },
+  {
+    "revision": "2f42774ee97ccd9e4c44a392bf36cd43",
+    "url": "/Visualization-infectious-diseases/static/media/logo.2f42774e.svg"
+  },
+  {
+    "revision": "b19fc25712048bc08ec539303a23dc6d",
+    "url": "/Visualization-infectious-diseases/static/media/thanks.b19fc257.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1bc432073c256ab932faee4472d4e803.js"
+  "/Visualization-infectious-diseases/precache-manifest.67f9e11b41607a3f06eb1edb297eff78.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Visualization-infectious-diseases/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
